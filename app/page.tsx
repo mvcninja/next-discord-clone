d033bd330dbd564fb3d3a53c24794a5c61f60ffb
mvcninja <mvcninja@texashomeresources.com>
2023-12-06 13:43:38 -0600

    home page now redirects when logged in

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
 import { ModeToggle } from "@/components/mode-toggle";
-import { UserButton } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+import { UserButton, auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = auth();
+
+  if (userId) {
+    const profile = await db.profile.findFirst({
+      where: { userId },
+      include: {
+        servers: {
+          where: { profile: { userId } }
+        }
+      }
+    });
+  
+    const serverId = profile?.servers[0].id;
+
+    if (serverId) {
+      return redirect(`/servers/${serverId}`);
+    }
+  }
+
   return (
     <main className='flex flex-col'>
-      Hello Discord Clone
+      <h1>Welcome to Encord!</h1>
       <UserButton afterSignOutUrl="/" />
       <ModeToggle />
     </main>
